b8ad23d96b44e39eef139755abe7140ff0a5fe76
Iuliia Lipatova <i.lipatova@godeltech.com>
2023-04-24 09:27:54 +0200

    minor fix

--- src/components/MenuControling/index.tsx
+++ src/components/MenuControling/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useAppSelector } from '../../redux/hooks';
+import { useAppSelector } from 'redux/hooks';
 import style from './index.module.scss';
 
 interface IProp {
