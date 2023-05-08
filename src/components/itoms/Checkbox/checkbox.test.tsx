df237af81ac3412cbf7594bee47c4c7a138afdbc
Iuliia Lipatova <i.lipatova@godeltech.com>
2023-05-08 08:20:35 +0200

    update test-checkbox

--- src/components/itoms/Checkbox/checkbox.test.tsx
+++ src/components/itoms/Checkbox/checkbox.test.tsx
@@ -1,21 +1,24 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Checkbox from './index';
 
 describe('test checkbox', () => {
+  const handleComplited = jest.fn();
   it('prerender correctly', () => {
-    const handleComplited = jest.fn();
     render(<Checkbox done={false} onComplited={handleComplited} id="001" />);
-    const input = document.querySelector('input');
+    const checkbox = screen.getByRole('checkbox');
+    userEvent.dblClick(checkbox);
+    expect(handleComplited).toBeCalledTimes(2);
+    expect(checkbox).not.toBeChecked();
     const label = document.querySelector('label');
+    expect(label).toBeInTheDocument();
     const span = document.querySelector('span');
     expect(span).toHaveStyle({
       background:
         'linear-gradient(to right, rgba(248, 13, 221, 1), rgba(40, 73, 237, 1)',
     });
-    expect(label).toBeInTheDocument();
-    expect(input).toBeInTheDocument();
-    if (input) fireEvent.change(input);
   });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
