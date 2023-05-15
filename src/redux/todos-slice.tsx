380c78c2148b7fcf1d0ff426f0771e389cc22588
Iuliia Lipatova <i.lipatova@godeltech.com>
2023-05-15 12:55:24 +0200

    minor update for style

--- src/redux/todos-slice.tsx
+++ src/redux/todos-slice.tsx
@@ -24,7 +24,7 @@ const todosSlice = createSlice({
       });
     },
     removeAllComplited: state => {
-      return state.filter(todo => todo.done === false);
+      return state.filter(todo => !todo.done );
     },
   },
 });
